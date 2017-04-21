3aaa7a8f13d736db3830eb99ad7b09971bdeffcc
Richard Kwon <richardkwon@hotmail.com>
2017-04-20 19:42:02 -0500

    add R source code to repo

---
+++ run_analysis.R
@@ -0,0 +1,8 @@
+run_analysis() <- function {
+    ##
+    ## Author: Richard Kwon
+    ## Date: April, 2017
+    ## Human Activity Recognition - Coursera (Getting and Cleaning Data course)
+    ##
+    
+}
